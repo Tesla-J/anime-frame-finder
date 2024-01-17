67d1294c2e0486cbe308ebcc65a1cedcb1f95457
Rafael Marcos <rafix995@gmail.com>
2024-01-17 23:45:31 +0100

    Image selection
    Deprecated aproach removed, now image selection use contracts to get a result from an activity.

--- app/src/main/java/ao/rafaelmarcos/animeimagefinder/MainActivity.java
+++ app/src/main/java/ao/rafaelmarcos/animeimagefinder/MainActivity.java
@@ -1,52 +1,44 @@
 package ao.rafaelmarcos.animeimagefinder;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-    FloatingActionButton fab;
-    ImageView toSearchImage;
+    FloatingActionButton mFab;
+    ImageView mToSearchImage;
 
-    // result code
-    private int SELECTION_RESULT_CODE = 200;
+
+    ActivityResultLauncher<String> mImageSelector = registerForActivityResult(new ActivityResultContracts.GetContent(),
+            new ActivityResultCallback<Uri>() {
+                @Override
+                public void onActivityResult(Uri o) {
+                    mToSearchImage.setImageURI(o);
+                }
+            });
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        fab = findViewById(R.id.fab);
-        toSearchImage = findViewById(R.id.to_search_image);
+        mFab = findViewById(R.id.fab);
+        mToSearchImage = findViewById(R.id.to_search_image);
 
         // searching image
-        fab.setOnClickListener(this::imageSelector);
+        mFab.setOnClickListener(this::imageSelector);
     }
 
     private void imageSelector(View v){
-        Intent intent = new Intent();
-        intent.setType("image/*");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-
-        // call image selector
-        startActivityForResult(intent, SELECTION_RESULT_CODE);
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data){
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if(resultCode == RESULT_OK && requestCode == SELECTION_RESULT_CODE){
-            Uri selectedImageUri = data.getData();
-
-            if(selectedImageUri != null)
-                toSearchImage.setImageURI(selectedImageUri);
-        }
+        mImageSelector.launch("image/*");
     }
 }
