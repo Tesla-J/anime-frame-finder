fbeaf2974ce878e30457ac5cbfcb3621c804a214
Rafael Marcos <rafix995@gmail.com>
2024-01-19 02:04:16 +0100

    Fixing compilation error for kotlin code

---
+++ app/src/main/java/ao/rafaelmarcos/animeimagefinder/SearchService.kt
@@ -0,0 +1,12 @@
+package ao.rafaelmarcos.animeimagefinder
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class SearchService : Service() {
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+}
