22a83cf7416c9e2981268bfa56d8e5a15c481971
Rafael Marcos <rafix995@gmail.com>
2024-01-20 22:21:59 +0100

    Fixing episode attribute type from Unit to Any

--- app/src/main/java/ao/rafaelmarcos/animeimagefinder/httprequest/Result.kt
+++ app/src/main/java/ao/rafaelmarcos/animeimagefinder/httprequest/Result.kt
@@ -2,10 +2,10 @@ package ao.rafaelmarcos.animeimagefinder.httprequest
 
 import java.net.URL
 
-data class ResultFormat(
+data class Result(
     val anilist: Long,
     val filename: String,
-    val episode: Unit?, // refer to aniep, IDK HOW TO REPRESENT THAT... YET
+    val episode: Any?, // refer to aniep, IDK HOW TO REPRESENT THAT... YET
     val from: Float,
     val to: Float,
     val similarity: Double,
